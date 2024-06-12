e3f8127726961ba0b58a922ccdd1f642da48fb69
dalvifaraz <dalvifaraz@gmail.com>
2024-06-13 00:25:46 +0530

    jwt-auth setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRouthes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://dalvifaraz:jwt-auth-admin@cluster0.s1e15zv.mongodb.net/';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+app.use(authRoutes);
---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+module.exports.signup_get = (req, res) => {
+    res.render('signup');
+}
+
+module.exports.login_get = (req, res) => {
+    res.render('login');
+}
+
+module.exports.signup_post = (req, res) => {
+    res.send('singnew up');
+}
+
+module.exports.login_post = (req, res) => {
+    res.send('user login');
+}
+
+module.exports.logout = (req, res) => {
+    res.send('user logout');
+}
