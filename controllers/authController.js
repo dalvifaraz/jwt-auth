71e4f975f909d6244fc8c759fb73efeac5406716
dalvifaraz <dalvifaraz@gmail.com>
2024-06-19 00:27:13 +0530

    add new user - signup controller to model(db)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -6,9 +8,15 @@ module.exports.login_get = (req, res) => {
     res.render('login');
 }
 
-module.exports.signup_post = (req, res) => {
-    console.log(req.body, 'request body');
-    res.send('singnew up', req);
+module.exports.signup_post = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+         const user = await User.create({ email, password });
+         res.status(201).json(user);
+    } catch (err) {
+        console.log('Error: ', err);
+        res.status(400).send('error occoured, user not created!');
+    }
 }
 
 module.exports.login_post = (req, res) => {
