97bbf1f9042e5a6b5d811c06496c7c51ec0e2e9b
dalvifaraz <dalvifaraz@gmail.com>
2024-06-20 00:03:19 +0530

    handling error in controller using mongoose

--- models/User.js
+++ models/User.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter an email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email']
+        // validate: [(email) => {
+        //     //try performing valid email
+        // }, 'Please enter a valid email']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 5,
-        maxLength: 12
+        required: [true, 'Please enter the password'],
+        minlength: [5, 'Password length should be between 5 - 12 character'],
+        maxlength: [12, 'Password length should be between 5 - 12 character'],
     },
 });
 
