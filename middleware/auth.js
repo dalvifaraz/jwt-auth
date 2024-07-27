f25b7dd1f84b099476e342cffc84e25915549151
dalvifaraz <dalvifaraz@gmail.com>
2024-07-28 01:08:55 +0530

    DAY12: Conditionally rendering user data and login/logout

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwtToken;
@@ -19,4 +20,25 @@ const requireAuth = (req, res, next) => {
     }
 };
 
-module.exports = { requireAuth };
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwtToken;
+    if (token) {
+        jwt.verify(token, 'this is my secret key', async (error, decodedToken) => {
+            if (error) {
+                console.log('token verification error: ', error.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log('token verified: ', decodedToken)
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+};
+
+module.exports = { requireAuth, checkUser };
