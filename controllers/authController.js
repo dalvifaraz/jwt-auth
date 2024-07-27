212627cb80ff19f00b99513387a0c6a86dd972a0
dalvifaraz <dalvifaraz@gmail.com>
2024-07-28 00:45:11 +0530

    DAY11: jwt token verification

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,7 +47,7 @@ module.exports.signup_post = async (req, res) => {
   try {
     const user = await User.create({ email, password });
     const token = createToken(user._id);
-    res.cookie('jwt-token', token, { httpOnly: true, maxAge: maxAge * 1000 });//maxage in milisecond
+    res.cookie('jwtToken', token, { httpOnly: true, maxAge: maxAge * 1000 });//maxage in milisecond
     res.status(201).json({ user: user._id});
   } catch (err) {
     const errors = handleError(err);
@@ -61,7 +61,7 @@ module.exports.login_post = async (req, res) => {
     const user = await User.login(email, password);
     //create a token and store it in cookie
     const token = createToken(user._id);
-    res.cookie('jwt-token', token, { httpOnly: true, maxAge: maxAge * 1000 });//maxage in milisecond
+    res.cookie('jwtToken', token, { httpOnly: true, maxAge: maxAge * 1000 });//maxage in milisecond
     res.status(200).json({ user: user._id });
   } catch (err) {
     const errors = handleError(err);
---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwtToken;
+    console.log('hello', req.cookies);
+
+    // check if jwt exist and verify is it correct
+    if (token) {
+        jwt.verify(token, 'this is my secret key', (error, decodedToken) => {
+            if (error) {
+                console.log('token verification error: ', error.message);
+                res.redirect('/login');
+            } else {
+                console.log('token verified: ', decodedToken)
+                next();
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+};
+
+module.exports = { requireAuth };
